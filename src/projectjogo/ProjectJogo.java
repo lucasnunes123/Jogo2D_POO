0ab0ee8fefea92929f52bd824e046c5e3dd75782
Lucas Nunes <lukasnunesgs@gmail.com>
2018-01-30 15:53:24 -0300

    uma pequena alteração,add pulo do personagem

--- src/projectjogo/ProjectJogo.java
+++ src/projectjogo/ProjectJogo.java
@@ -1,33 +1,31 @@
-package projectjogo;
+package ProjectJogo;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.animation.TranslateTransition;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
- *
  * @author Lucas Nunes GS
  */
+
 public class ProjectJogo extends Application {
     
-    private int posX = 25, posY = 250; 
-    private int mov = 0;
+    private int posX = 25; 
+    private final  int posY = 310;
     
-    private Image imageD = new Image("carrinha.png");
-    private Image imageE = new Image("carinha2.png");
-    private Image nativoima = new Image("giphy.gif");
-    private Image cena = new Image("original.png");
+    private final Image imageD = new Image("Imagens/carrinha.png");
+    private final Image imageE = new Image("Imagens/carinha2.png");
+    private final Image nativoima = new Image("Imagens/giphy.gif");
+    private final Image cena = new Image("Imagens/Syberian.png");
             
     @Override
     public void start(Stage primaryStage) {
@@ -40,51 +38,77 @@ public class ProjectJogo extends Application {
         Rectangle personagem = new Rectangle();
         Rectangle nativo = new Rectangle();
         
+        //pulo_personagem
+        TranslateTransition pulopersonagem = new TranslateTransition(Duration.millis(400),personagem);
+        pulopersonagem.setByY(-100f);
+        pulopersonagem.setCycleCount((int) 2f);
+        pulopersonagem.setAutoReverse(true);
+        //animação_nativo
+        TranslateTransition anima_nativo = new TranslateTransition(Duration.millis(3500),nativo);
+        anima_nativo.setByX(500f);
+        anima_nativo.setByX(-500f);
+        anima_nativo.setCycleCount((int) 1f);
+        anima_nativo.setAutoReverse(true);
+        anima_nativo.play();
+
         //Config_personagem
-        personagem.setX(posX); // distancia da borda de cima
-        personagem.setY(posY); // distancia da borda da esquerda
-        personagem.setWidth(40); // largura
-        personagem.setHeight(50); // altura
+        personagem.setX(posX); // Posição inicial personagem
+        personagem.setY(posY); //Posião inicial personagem
+        personagem.setWidth(40); // largura personagem
+        personagem.setHeight(50); // altura personagem
         personagem.setFill(new ImagePattern(imageD)); // preenchimento
         
         //Config_nativo
         nativo.setX(500);//Posição inicial nativo
-        nativo.setY(posY);
-        nativo.setWidth(70);//Largura
-        nativo.setHeight(60);//Altura
+        nativo.setY(posY);//Posição inicial nativo
+        nativo.setWidth(70);//Largura nativo
+        nativo.setHeight(60);//Altura nativo
         nativo.setFill(new ImagePattern(nativoima));
         
         root.getChildren().add(personagem);
         root.getChildren().add(nativo);
-        
+  
+        //evento de tecla precionada
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
+           @Override
+            
             //Movimento_personagens
             public void handle(KeyEvent event) {
-               
+                if(event.getCode().equals(KeyCode.UP)){
+                    pulopersonagem.play();
+                }
                 if(event.getCode().equals(KeyCode.LEFT)){
-                    posX = posX - 20; 
+                    if(posX < 1){
+                        posX = 0;         
+                    }
+                
+                   personagem.setX(posX);
+                   posX = posX - 10; 
                    personagem.setX(posX);
                    personagem.setFill(new ImagePattern(imageE));
                 }
                 if(event.getCode().equals(KeyCode.RIGHT)){
-                    posX = posX + 20;
+                    if(posX > 760){
+                        posX = 760;
+                    }
+                    personagem.setX(posX);
+                    posX = posX + 10;
                     personagem.setX(posX);
                     personagem.setFill(new ImagePattern(imageD));
-
                 }
+                if(event.getCode().equals(KeyCode.ESCAPE)){
+                    primaryStage.close();
+                }
+ 
             }
             
         });
         
-        
-        primaryStage.setTitle("Metal Slug:Versão lukita");
+        primaryStage.setTitle("Metal Chucrute:Versão lukita");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
     public static void main(String[] args) {
         launch(args);
-    }
-    
+    }   
 }
